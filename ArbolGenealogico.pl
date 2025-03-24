979baf06562cd1e03b73c9c566eb29c3bcc2bbde
Fredo <fredsotoucsm@gmail.com>
2025-03-23 22:14:46 -0500

    "avance actividad 01"

---
+++ ArbolGenealogico.pl
@@ -0,0 +1,55 @@
+hombre(fredy).
+hombre(davidyo).
+hombre(adri).
+hombre(luis).
+hombre(beto).
+
+hombre(angel).
+hombre(roberto).
+
+hombre(joel).
+hombre(elias).
+hombre(diego).
+hombre(abid).
+hombre(angelito).
+
+mujer(paty).
+mujer(faustina).
+mujer(glenda).
+
+mujer(yola).
+mujer(mary).
+mujer(areli).
+
+mujer(luz).
+mujer(loana).
+
+
+casados(fredy,paty). casados(luis,faustina).  casados(beto,glenda).
+casados(angel,mary). casados(roberto,yola).
+
+es_papa_de(luis,fredy). es_papa_de(luis,mary). es_papa_de(luis,yola).
+es_papa_de(beto,paty). es_papa_de(beto,areli).
+es_papa_de(fredy,davidyo). es_papa_de(fredy,adri).
+es_papa_de(angel,luz). es_papa_de(angel,angelito).
+es_papa_de(roberto,joel). es_papa_de(roberto,elias). es_papa_de(roberto,diego).
+
+es_mama_de(faustina,fredy). es_mama_de(faustina,mary). es_mama_de(faustina,yola).
+es_mama_de(glenda,paty). es_mama_de(glenda,areli).
+es_mama_de(paty,davidyo). es_mama_de(paty,adri).
+es_mama_de(mary,luz). es_mama_de(mary,angelito).
+es_mama_de(yola,joel). es_mama_de(yola,elias). es_mama_de(yola,diego).
+es_mama_de(areli,abid). es_mama_de(areli,loana).
+
+es_hijo_de(X,Y) :- hombre(X), (es_papa_de(Y,X);es_mama_de(Y,X)).
+es_hija_de(X,Y) :- mujer(X), (es_papa_de(Y,X);es_mama_de(Y,X)).
+
+es_hermano_de(X,Y) :- hombre(X), es_papa_de(Z,X), es_papa_de(Z,Y), X \= Y,
+    write(X), write(' y '), write(Y), write(' son hermanos.').
+es_hermano_de(X,Y) :- hombre(X), es_mama_de(Z,X), es_mama_de(Z,Y), X \= Y,
+    write(X), write(' y '), write(Y), write(' son hermanos.').
+
+es_hermana_de(X,Y) :- mujer(X), es_papa_de(Z,X), es_papa_de(Z,Y), X \= Y,
+    write(X), write(' y '), write(Y), write(' son hermanos.').
+es_hermana_de(X,Y) :- mujer(X), es_mama_de(Z,X), es_mama_de(Z,Y), X \= Y,
+    write(X), write(' y '), write(Y), write(' son hermanos.').
